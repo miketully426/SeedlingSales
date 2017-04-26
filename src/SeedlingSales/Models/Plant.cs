176d1564a48e94b0e68e9bd5729fd0723b36f4e4
miketully426 <mike.m.tully@gmail.com>
2017-04-25 23:22:23 -0500

    created Plant class and Add view

---
+++ src/SeedlingSales/Models/Plant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeedlingSales.Models
+{
+    public class Plant
+    {
+        public string Name { get; set; }
+        public int Seeded { get; set; }
+        public int Ready { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
