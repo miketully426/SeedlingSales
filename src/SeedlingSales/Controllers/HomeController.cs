6452f0f511d2e9ad9cb967505b21efce4ef85281
miketully426 <mike.m.tully@gmail.com>
2017-04-27 00:48:15 -0500

    Began db ties

--- src/SeedlingSales/Controllers/HomeController.cs
+++ src/SeedlingSales/Controllers/HomeController.cs
@@ -3,29 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SeedlingSales.Models;
+using SeedlingSales.ViewModels;
+using SeedlingSales.Data;
 
 namespace SeedlingSales.Controllers
 {
     public class HomeController : Controller
     {
+        
+        private readonly PlantDbContext context;
+
+        public HomeController(PlantDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        
+
         public IActionResult Index()
         {
+            
+            
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Add()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            AddViewModel viewModel = new AddViewModel();
+            return View(viewModel);
         }
 
-        public IActionResult Contact()
+        [HttpPost]
+        public IActionResult Add(AddViewModel newPlant)
         {
-            ViewData["Message"] = "Your contact page.";
+            Plant addPlant = new Models.Plant
+            {
+                Name = newPlant.Name,
+                Seeded = newPlant.Seeded,
+                Ready = newPlant.Ready,
+                Price = newPlant.Price
+                
+            };
+            context.Plants.Add(addPlant);
+            context.SaveChanges();
 
-            return View();
+
+
+            return Redirect("/");
         }
+
 
         public IActionResult Error()
         {
---
+++ src/SeedlingSales/Data/PlantDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SeedlingSales.Models;
+
+namespace SeedlingSales.Data
+{
+    public class PlantDbContext : DbContext
+    {
+        public DbSet<Plant> Plants { get; set; }
+
+        public PlantDbContext(DbContextOptions<PlantDbContext> options) : base(options) { }
+
+
+    }
+}
